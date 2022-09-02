40c63d443ad2ae489d0a17de0dce9e8743ebd7f1
Roberto <mntldss@gmail.com>
2022-09-01 22:53:52 -0400

    update functions and .env edited
    lambdas functions names changed to kebab case, env vars used in serverless.yml

--- src/lambdas/delete-video.js
+++ src/lambdas/delete-video.js
@@ -1,9 +1,8 @@
 const AWS = require("aws-sdk");
 const Response = require("../responses");
 
-const getVideo = async (event) => {
+const deleteVideoById = async (event) => {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
   const { id } = event.pathParameters;
 
   try {
@@ -17,12 +16,19 @@ const getVideo = async (event) => {
     if (!data.Item) {
       return Response.send(404,{message:"Video not found"});
     }
+    
+    await dynamoDb
+      .delete({
+        TableName: process.env.DB_TABLE_NAME,
+        Key: { id },
+      })
+      .promise();
 
-    return Response.send(200, data.Item);
+    return Response.send(200, {message: "Video deleted Successfuly"});
   } catch (error) {
     console.log(error);
     return Response.send(500, error);
   }
 };
 
-module.exports = { getVideo };
+module.exports = { deleteVideoById };
--- src/lambdas/get-video.js
+++ src/lambdas/get-video.js
@@ -1,8 +1,9 @@
 const AWS = require("aws-sdk");
 const Response = require("../responses");
 
-const deleteVideo = async (event) => {
+const getVideoById = async (event) => {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
   const { id } = event.pathParameters;
 
   try {
@@ -16,19 +17,12 @@ const deleteVideo = async (event) => {
     if (!data.Item) {
       return Response.send(404,{message:"Video not found"});
     }
-    
-    await dynamoDb
-      .delete({
-        TableName: process.env.DB_TABLE_NAME,
-        Key: { id },
-      })
-      .promise();
 
-    return Response.send(200, {message: "Video deleted Successfuly"});
+    return Response.send(200, data.Item);
   } catch (error) {
     console.log(error);
     return Response.send(500, error);
   }
 };
 
-module.exports = { deleteVideo };
+module.exports = { getVideoById };
